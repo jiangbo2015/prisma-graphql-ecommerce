18fae7f21d1fc1540cb20b7de80e4e8cad04daa5
jiangbo11 <jiangbo11@meituan.com>
2021-06-05 18:07:22 +0800

    feat: :sparkles: storefront

--- packages/storefront/pages/_app.tsx
+++ packages/storefront/pages/_app.tsx
@@ -3,12 +3,48 @@ import client from '../client'
 
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+import React from 'react'
+import PropTypes from 'prop-types'
+import Head from 'next/head'
+import { ThemeProvider } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+// import theme from '../src/theme';
+import { createMuiTheme, ThemeOptions } from '@material-ui/core/styles'
+
+const theme = createMuiTheme({})
+
+export default function MyApp(props) {
+    const { Component, pageProps } = props
+
+    React.useEffect(() => {
+        // Remove the server-side injected CSS.
+        const jssStyles = document.querySelector('#jss-server-side')
+        if (jssStyles) {
+            jssStyles.parentElement.removeChild(jssStyles)
+        }
+    }, [])
+
     return (
-        <ApolloProvider client={client}>
-            <Component {...pageProps} />
-        </ApolloProvider>
+        <React.Fragment>
+            <Head>
+                <title>My page</title>
+                <meta
+                    name="viewport"
+                    content="minimum-scale=1, initial-scale=1, width=device-width"
+                />
+            </Head>
+            <ApolloProvider client={client}>
+                <ThemeProvider theme={theme}>
+                    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+                    <CssBaseline />
+                    <Component {...pageProps} />
+                </ThemeProvider>
+            </ApolloProvider>
+        </React.Fragment>
     )
 }
 
-export default MyApp
+MyApp.propTypes = {
+    Component: PropTypes.elementType.isRequired,
+    pageProps: PropTypes.object.isRequired,
+}
