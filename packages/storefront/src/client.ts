e7b50b84d28a576c55458f4d60773a118d72bdac
jiangbo2015 <772051431@qq.com>
2021-11-03 22:15:29 +0800

    feat(storefront): global error handle

--- packages/storefront/src/client.ts
+++ packages/storefront/src/client.ts
@@ -1,14 +1,45 @@
-import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
+import { ApolloClient, HttpLink, InMemoryCache, from, Observable } from '@apollo/client'
 
-const client = new ApolloClient({
-    cache: new InMemoryCache(),
-    ssrMode: true,
-    link: new HttpLink({
+import { onError } from '@apollo/client/link/error'
+
+// Log any GraphQL errors or network error that occurred
+const errorLink = onError(
+    ({ graphQLErrors, networkError, operation, forward }) => {
+        if (graphQLErrors) {
+            graphQLErrors.forEach(
+                ({ message, locations, path, extensions }) => {
+                    console.log(
+                        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+                    )
+
+                    // if UNAUTHENTICATED, go to login page
+                    if (extensions?.code === 'UNAUTHENTICATED') {
+                        window.location.assign('/login')
+                    }
+                }
+            )
+            return Observable.of(operation)
+        }
+
+        if (networkError) {
+            console.log(`[Network error]: ${networkError}`)
+            return Observable.of(operation)
+        }
+        return forward(operation)
+    }
+)
+
+const httpLink = new HttpLink({
         uri:
             process.env.NODE_ENV === 'development'
                 ? 'http://localhost:4000'
                 : 'https://pgfe-server.herokuapp.com',
-    }),
+    })
+
+const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    ssrMode: true,
+    link: from([errorLink, httpLink]),
 
     connectToDevTools: true,
 })
